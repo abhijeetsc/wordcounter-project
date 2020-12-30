455f229d6f144c6f522a2952508cb6d3b4719585
Abhijeet Singh Chouhan <chouhanabhijeet7@gmail.com>
2020-12-30 10:33:58 +0530

    Removed secret key.

--- wordcounter/settings.py
+++ wordcounter/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')o1d(f6%y(5hz43ai5$juo4%kf=+^6^jeqj+@rr@yg!)q5r30n'
+SECRET_KEY = 'SECRET_KEY_HERE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
